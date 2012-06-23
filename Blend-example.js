5b8fc4f89bea17293d505c7ae77dbd5bd74af337
Aurélien Scoubeau <aurelien.scoubeau@gmail.com>
2012-06-23 22:39:12 +0200

    Refactored the demo page using jQuery, to simplify the whole stuff.

--- Blend-example.js
+++ Blend-example.js
@@ -50,90 +50,85 @@ function addCanvasStep(canvas) {
 }
 
 // let overwrite existing FX to insert the canvas into the DOM
-function circleMask(ctx, amount) {
+var desaturate = Blend.factory.fx('context', function(ctx, amount) {
+	Blend.fx.desaturate(ctx, amount);
 	addCanvasStep.call(this, ctx.canvas);
-	return Blend.fx.circleMask(ctx, amount);
-}
-function desaturate(ctx, amount) {
+});
+var circleMask = Blend.factory.fx('context', function(ctx, data) {
+	Blend.fx.circleMask(ctx, data);
 	addCanvasStep.call(this, ctx.canvas);
-	return Blend.fx.desaturate(ctx, amount);
-}
-function alpha(ctx, amount) {
+});
+var alpha = Blend.factory.fx('pixels', function(pixels, data) {
+	var canvas = Blend.factory.canvas(pixels.width, pixels.height);
+	var ctx = canvas.getContext('2d');
+	Blend.fx.alpha(pixels, data);
+	ctx.putImageData(pixels, 0, 0);
 	addCanvasStep.call(this, ctx.canvas);
-	return Blend.fx.alpha(ctx, amount);
-}
+});
 
 // callbacks to apply on images after loading
-var callbacks = [];
+// var f3 = function(img) {
+// 	var grid = new Blend.map.Grid2D(3, 3, [1/8, 1/4, 1/2, 3/4, 1, 3/4, 1/2, 0, 1/2]);
+// 	var blender = new Blend.create(img, grid);
+// 	blender.fx(circleMask).fx(desaturate);
+// 	blender.map.removeCol();
+// 	blender.map.removeCol();
+// 	blender.map.removeRow();
+// 	blender.map.removeRow();
+// 	blender.fx(circleMask).update();
+// }
 
-var f1 = function(img) {
-	var grid = Blend.map.Grid2D(1, 1, [1/4]);
-	var blender = Blend.create(img, grid);
-	blender.fx(alpha).fx(circleMask).update();
-}
-callbacks.push(f1);
+// var f4 = function(img) {
+// 	var tree = Blend.map.Tree2D([
+// 		[										1],
+// 		[					1/2, 									1/2],
+// 		[		undefined, 			undefined, 				1/4, 				1/4],
+// 		[undefined, undefined, undefined, undefined, undefined, undefined, 1/8, 1/8]
+// 	]);
+// 	var blender = Blend.create(img, tree);
+// 	blender.fx(desaturate).fx(circleMask).update();
+// }
 
-var f2 = function(img) {
-	var grid = Blend.map.Grid2D(2, 1, [1/2, 1/2]);
-	var blender = Blend.create(img, grid);
-	blender.fx(circleMask).fx(alpha).update();
-}
-callbacks.push(f2);
+$(document).ready(function() {
+	// preload images
+	var blends = [{
+		source: 'qur2.jpg',
+		title: 'Single area & 2 effects',
+		grid: new Blend.map.Grid2D(1, 1, [1/4]),
+		fxQueue: [alpha, circleMask]
+	}, {
+		source: 'qur2.jpg',
+		title: '2 areas & 2 effects',
+		grid: new Blend.map.Grid2D(2, 1, [1/2, 1/2]),
+		fxQueue: [circleMask, alpha]
+	}, {
+		source: 'qur2.jpg',
+		title: 'Multiple areas and multiple effects',
+		grid: new Blend.map.Grid2D(2, 2, [1/4, 3/4, 3/4, 1/4]),
+		fxQueue: [circleMask, desaturate]
+	// }, {
+	// 	source: 'qur2.jpg',
+	// 	title: 'Irregular areas and multiple effects'
+	}];
 
-var f3 = function(img) {
-	var grid = Blend.map.Grid2D(3, 3, [1/8, 1/4, 1/2, 3/4, 1, 3/4, 1/2, 0, 1/2]);
-	var blender = Blend.create(img, grid);
-	blender.fx(circleMask).fx(desaturate);
-	blender.map.removeCol();
-	blender.map.removeCol();
-	blender.map.removeRow();
-	blender.map.removeRow();
-	blender.fx(circleMask).update();
-}
-callbacks.push(f3);
+	var blendTpl = $('#blendTpl').template(),
+		root = $('ul').parent();
 
-var f4 = function(img) {
-	var tree = Blend.map.Tree2D([
-		[										1],
-		[					1/2, 									1/2],
-		[		undefined, 			undefined, 				1/4, 				1/4],
-		[undefined, undefined, undefined, undefined, undefined, undefined, 1/8, 1/8]
-	]);
-	var blender = Blend.create(img, tree);
-	blender.fx(desaturate).fx(circleMask).update();
-}
-callbacks.push(f4);
-
-// callback handling images after loading
-function imgLoadHandler(images) {
-	var els = document.getElementById('content').getElementsByTagName('h2');
-	for (var i in images) {
-		var img = images[i], container = els[i].parentNode, f = callbacks[i];
-		insertDomElem(container, 'h4', 'Original and Result :');
-		insertDomElem(container, 'img', false, {'src' : 'qur2.jpg'});
-		container.appendChild(img);
-		insertDomElem(container, 'h4', 'Computation steps :');
-		insertDomElem(container, 'div', false, {'className' : 'clear'});
-		f(img);
+	for (var i=0, bound=blends.length; i<bound; i++) {
+		blendHelper(blends[i]);
 	}
-}
 
-// helper function to add an HTML tag more easily
-function insertDomElem(parent, tag, inner, attr) {
-	var el = document.createElement(tag);
-	if (inner)
-		el.appendChild(document.createTextNode(inner));
-	for (var a in attr)
-		el[a] = attr[a];
-	parent.appendChild(el)
-}
-
-// define vars for preloader
-var sources = ['qur2.jpg', 'qur2.jpg', 'qur2.jpg', 'qur2.jpg'];
-var ip = null;
-
-// callback handling body after loading
-function bodyLoadHandler(sources) {
-	var ip = new ImagePreloader(sources, imgLoadHandler);
-// 	// if (sources.length == count)
-}
+	function blendHelper(blend) {
+		var tpl = $.tmpl(blendTpl, blend).appendTo(root);
+		var img = tpl.find('img:last').attr('src', blend.source);
+		if (blend.fxQueue && blend.fxQueue.length) {
+			img.one('load', function() {
+				var fxQueue = blend.fxQueue,
+					blender = new Blend.create(img.get(0), blend.grid);
+				for (var j=0, jmax=fxQueue.length; j<jmax; j++)
+					blender.fx(fxQueue[j]);
+				blender.update();
+			});
+		}
+	}
+});
