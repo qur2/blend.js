95f5a2e41c3f87741a91e9127603cf150715c298
Aurélien Scoubeau <aurelien.scoubeau@gmail.com>
2012-06-23 22:36:55 +0200

    Refactored the whole library:  - Now relies on prototyping.  - Provides fx builders to seamlessly apply effects on pixels or contexts.  - Improved Grid2D iterator.  - ! Tree2D is NOT refactored yet and is broken atm !

--- Blend.js
+++ Blend.js
@@ -1,109 +1,112 @@
-Blend = {map : {}, fx : {}};
-// todo hold more info in each cell and pass it to the effect functions ?
-// todo specify only 1 dim and compute the 2nd as data.length / dim
-Blend.map.Grid2D = function(w, h, data) {
-	if (w*h != data.length)
-		throw "given dimensions doesn't match member count"
+Blend = {map: {}, fx: {}, factory: {}};
 
-	Iterator = function(tree) {
-		return {
-			grid : grid,
-			current : -1,
+Blend.map.Grid2D = function(width, height, data) {
+	if (!width) width = data.length / height;
+	if (!height) height = data.length / width;
+	if (data.length != width*height || Math.floor(height)*Math.floor(width) != width*height)
+		throw "given dimensions doesn't match member count";
 
-			hasNext : function() {
-				return this.current < this.grid.data.length-1;
-			},
-			next : function() {
-				this.current++;
-				return {
-					x : this.current%this.grid.width,
-					y : Math.floor(this.current/this.grid.width),
-					data : this.grid.data[this.current],
-					i : this.current
-				};
-			},
-			reset : function() {
-				this.current = -1;
-			}
-		}
+	this.data = data;
+	this.width = width;
+	this.height = height;
+	this.projectedWidth = 1;
+	this.projectedHeight = 1;
+};
+
+
+var proto = {
+	iterator: function() {
+		var current = -1;
+		var me = this;
+		hasNext = function() {
+			return current < this.data.length-1;
+		};
+		next = function() {
+			return this.cell(++current);
+		};
+		return function() {
+			return hasNext.call(me) ? next.call(me) : false;
+		};
+	},
+
+	addRow: function(row, i) {
+		this.checkRowLength(row);
+		i = ('undefined' == typeof i) ? this.height : Math.max(0, Math.min(i, this.height));
+		this.data.slice(i*this.width, 0, row);
+		this.height++;
+		return this;
+	},
+	removeRow: function(i) {
+		var row = this.data.splice(i*this.width, this.width);
+		this.height--;
+		return row;
+	},
+
+	addCol: function(col, i) {
+		this.checkColLength(col);
+		i = ('undefined' == typeof i) ? this.width: Math.max(0, Math.min(i, this.width));
+		for (var j=this.height-1; j>=0; j--)
+			this.data.splice(j*this.width+i, 0, col.pop());
+		this.width++;
+		return this;
+	},
+	removeCol: function(i) {
+		col = [];
+		for (var j=this.height-1; j>=0; j--)
+			col.unshift(this.data.splice(j*this.width+i, 1).pop());
+		this.width--;
+		return col;
+	},
+
+	checkRowLength: function(row) {
+		if (row.length != this.width)
+			throw "row length doesn't match column number";
+	},
+	checkColLength: function(col) {
+		if (col.length != this.height)
+			throw "column length doesn't match row number";
+	},
+	checkGridIndex: function(index) {
+		if (index >= this.data.length)
+			throw "cell index out of bound";
+	},
+
+	project: function(width, height) {
+		this.projectedWidth = width;
+		this.projectedHeight = height;
+	},
+
+	cell: function(index) {
+		this.checkGridIndex(index);
+		var data = this.data[index];
+		var i = index%this.width,
+			j = Math.floor(index/this.width);
+		var areaWidth = this.projectedWidth / this.width,
+			areaHeight = this.projectedHeight /this.height;
+		var wFloat = Math.floor(areaWidth) != areaWidth,
+			hFloat = Math.floor(areaHeight) != areaHeight;
+		var wInc = (wFloat && i%this.width == this.width-1) ? 1: 0,
+			hInc = (hFloat && Math.floor(i/this.height) == this.height-1) ? 1: 0;
+		var area = {
+			x: Math.floor(areaWidth * i),
+			y: Math.floor(areaHeight * j),
+			w: Math.floor(this.projectedWidth / this.width)+wInc,
+			h: Math.floor(this.projectedHeight /this.height)+hInc,
+			data: data
+		};
+		return area;
+	},
+
+	toString: function() {
+		repr = [];
+		for (var i=0; i<this.height; i++)
+			repr.push(this.data.slice(i*this.width, (i+1)*this.width).join(' '));
+		return '[' + repr.join('\n ') + ']';
 	}
+};
 
-	var grid = {
-		data : data,
-		width : w,
-		height : h,
-		projectedWidth : 1,
-		projectedHeight : 1,
-		addRow : function(row, i) {
-			this.checkRowLength(row);
-			i = ('undefined' == typeof i) ? this.height : Math.max(0, Math.min(i, this.height));
-			Array.prototype.splice.apply(this.data, [i*this.width, 0].concat(row));
-			this.height++;
-			return this;
-		},
-		removeRow : function(i) {
-			var row = Array.prototype.splice.apply(this.data, [i*this.width, this.width]);
-			this.height--;
-			return row;
-		},
-
-		addCol : function(col, i) {
-			this.checkColLength(col);
-			i = ('undefined' == typeof i) ? this.width : Math.max(0, Math.min(i, this.width));
-			for (var j=this.height-1; j>=0; j--)
-				this.data.splice(j*this.width+i, 0, col.pop());
-			this.width++;
-			return this;
-		},
-		removeCol : function(i) {
-			col = [];
-			for (var j=this.height-1; j>=0; j--)
-				col.unshift(this.data.splice(j*this.width+i, 1).pop());
-			this.width--;
-			return col;
-		},
-
-		checkRowLength : function(row) {
-			if (row.length != this.width)
-				throw "row length doesn't match column number";
-		},
-		checkColLength : function(col) {
-			if (col.length != this.height)
-				throw "column length doesn't match row number";
-		},
-
-		project : function(width, height) {
-			this.projectedWidth = width;
-			this.projectedHeight = height;
-		},
-
-		nextArea : function() {
-			var areaWidth = this.projectedWidth / this.width,
-				areaHeight = this.projectedHeight /this.height;
-			var wFloat = Math.floor(areaWidth) != areaWidth,
-				hFloat = Math.floor(areaHeight) != areaHeight;
-			var node = this.iterator.hasNext() ? this.iterator.next() : false;
-			var wInc = (wFloat && node.i%this.width == this.width-1) ? 1 : 0,
-				hInc = (hFloat && Math.floor(node.i/this.height) == this.height-1) ? 1 : 0;
-			var area = {
-				x : Math.floor(areaWidth * node.x),
-				y : Math.floor(areaHeight * node.y),
-				w : Math.floor(this.projectedWidth / this.width)+wInc,
-				h : Math.floor(this.projectedHeight /this.height)+hInc,
-				data : node.data
-			};
-			return area;
-		},
-
-		toString : function() {
-			repr = [];
-			for (var i=0; i<this.height; i++)
-				repr.push(this.data.slice(i*this.width, (i+1)*this.width).join(' '));
-			return '[' + repr.join('\n ') + ']';
-		}
-	}
-	grid.iterator = Iterator(grid);
-	return grid;
+for (var member in proto) {
+	Blend.map.Grid2D.prototype[member] = proto[member];
 }
 
 /**
@@ -117,10 +120,10 @@ Blend.map.Tree2D = function(data) {
 
 	Iterator = function(tree) {
 		return {
-			tree : tree,
-			current : -1,
+			tree: tree,
+			current: -1,
 
-			hasNext : function() {
+			hasNext: function() {
 				return this.current < this.tree.leaves.length-1;
 			},
 			/**
@@ -131,7 +134,7 @@ Blend.map.Tree2D = function(data) {
 			 * in the tree, we know some consecutive leaves (i.e. right siblings) can be skipped
 			 * as it would lead to the same ancestor.
 			 */
-			next : function() {
+			next: function() {
 				this.current++;
 				// console.log(this.current);
 				var i = this.tree.data.length-1;
@@ -145,48 +148,48 @@ Blend.map.Tree2D = function(data) {
 					// some nodes can be skipped as any node has 0 or 2 children
 					this.current += (tree.data.length-i-2)*2-1;
 					// console.log('up', tree.data.length-i-2, 'right', (tree.data.length-i-2)*2-1, this.current);
-					return {x : this.current-1, y : i+1, data : leaf};
+					return {x: this.current-1, y: i+1, data: leaf};
 				}
 				else
-					return {x : this.current, y : i, data : leaf};
+					return {x: this.current, y: i, data: leaf};
 			},
-			reset : function() {
+			reset: function() {
 				this.current = -1;
 			}
-		}
-	}
+		};
+	};
 
 	var tree = {
-		dim : treeDim,
-		data : data,
-		leaves : data[data.length-1],
-		projectedWidth : 1,
-		projectedHeight : 1,
+		dim: treeDim,
+		data: data,
+		leaves: data[data.length-1],
+		projectedWidth: 1,
+		projectedHeight: 1,
 
-		project : function(width, height) {
+		project: function(width, height) {
 			this.projectedWidth = width;
 			this.projectedHeight = height;
 		},
 
-		nextArea : function() {
-			var node = this.iterator.hasNext() ? this.iterator.next() : false;
+		nextArea: function() {
+			var node = this.iterator.hasNext() ? this.iterator.next(): false;
 			var hSplit = Math.ceil(node.y/2), vSplit = Math.floor(node.y/2);
 			var w = this.projectedWidth/Math.pow(2, vSplit), h = this.projectedHeight/Math.pow(2, hSplit);
 			var area = {
 				// the module adujsts the position if the spitting direction is relevant
-				x : w * (hSplit-1 + ((hSplit==vSplit)?node.x%2:0)),
-				y : h * (node.y-1 + ((hSplit>vSplit)?node.x%2:0)),
-				w : w,
-				h : h,
-				data : node.data
+				x: w * (hSplit-1 + ((hSplit==vSplit)?node.x%2:0)),
+				y: h * (node.y-1 + ((hSplit>vSplit)?node.x%2:0)),
+				w: w,
+				h: h,
+				data: node.data
 			};
 			// console.log(hSplit, vSplit, node.x, node.y, area);
 			return area;
 		}
-	}
+	};
 	tree.iterator = Iterator(tree);
 	return tree;
-}
+};
 
 Blend.fx.desaturate = function(ctx, amount) {
 	var pixels = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
@@ -197,7 +200,7 @@ Blend.fx.desaturate = function(ctx, amount) {
 		pixels.data[i+2] += (avg - pixels.data[i+2]) * amount;
 	}
 	ctx.putImageData(pixels, 0, 0);
-}
+};
 Blend.fx.invert = function(ctx, amount) {
 	var pixels = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
 	for (var i=0; i<pixels.data.length; i+=4) {
@@ -206,13 +209,12 @@ Blend.fx.invert = function(ctx, amount) {
 		pixels.data[i+2] = 255 - pixels.data[i+2];
 	}
 	ctx.putImageData(pixels, 0, 0);
-}
-Blend.fx.alpha = function(ctx, amount) {
-	var pixels = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
+};
+Blend.fx.alpha = function(pixels, amount) {
 	for (var i=0; i<pixels.data.length; i+=4)
 		pixels.data[i+3] = (1-amount) * 255;
 	ctx.putImageData(pixels, 0, 0);
-}
+};
 Blend.fx.border = function(ctx, amount) {
 	var w = ctx.canvas.width,
 		h = ctx.canvas.height,
@@ -220,7 +222,7 @@ Blend.fx.border = function(ctx, amount) {
 	ctx.strokeStyle = '#FF2356';
 	for (var i=0; i<thickness; i++)
 		ctx.strokeRect(i, i, w-2*i, h-2*i);
-}
+};
 Blend.fx.circleMask = function(ctx, amount) {
 	var w = ctx.canvas.width,
 		h = ctx.canvas.height;
@@ -231,54 +233,62 @@ Blend.fx.circleMask = function(ctx, amount) {
 	ctx.fill();
 	ctx.globalCompositeOperation = 'destination-over';
 	ctx.fillRect(0, 0, w, h);
+};
+
+Blend.factory.fx = function(type, effect) {
+	if ('context' == type)
+		return function(x, y, w, h, data) {
+			var tmpCanvas = Blend.factory.canvas(w, h);
+			var tmpContext = tmpCanvas.getContext('2d');
+			tmpContext.drawImage(this.canvas, x, y, w, h, 0, 0, w, h);
+			effect.call(this, tmpContext, data);
+			var pixels = tmpContext.getImageData(0, 0, w, h);
+			this.context.putImageData(pixels, x, y);
+		};
+	else if ('pixels' == type)
+		return function(x, y, w, h, data) {
+			var pixels = this.context.getImageData(x, y, w, h);
+			effect.call(this, pixels, data);
+			this.context.putImageData(pixels, x, y);
+		};
+};
+Blend.factory.canvas = function(width, height) {
+	var canvas = document.createElement('canvas');
+	canvas.width = width;
+	canvas.height = height;
+	return canvas;
 }
 
 Blend.create = function(img, map) {
-	canvas = document.createElement('canvas');
-	canvas.height = img.height;
-	canvas.width = img.width;
-	context = canvas.getContext('2d');
+	var canvas = Blend.factory.canvas(img.width, img.height);
+	var context = canvas.getContext('2d');
 	context.drawImage(img, 0, 0);
 	map.project(img.width, img.height);
 
-	return {
-		img : img,
-		map : map,
-		canvas : canvas,
-		context : context,
+	this.img = img;
+	this.map = map;
+	this.canvas = canvas;
+	this.context = context;
+};
 
-		fx : function(effect) {
-			function _fx(x, y, w, h, amount) {
-				// var pixels = this.context.getImageData(x, y, Math.floor(w), Math.floor(h));
-				// effect.call(this, pixels, amount, this.context);
-				// this.context.putImageData(pixels, x, y);
-				// console.log(x, y, w, h, amount);
-				var tmpCanvas = document.createElement('canvas');
-				tmpCanvas.width = w;
-				tmpCanvas.height = h;
-				var tmpContext = tmpCanvas.getContext('2d');
-				tmpContext.drawImage(this.canvas, x, y, w, h, 0, 0, w, h);
-
-				// var result = effect.call(this, x, y, w, h, amount, this.context);
-				// if ('object' == typeof result && result.width && result.height && result.data)
-				// 	this.context.putImageData(result, x, y);
-				effect.call(this, tmpContext, amount);
-				// this.context.drawImage(tmpCanvas, x, y);
-				var tmpImageData = tmpContext.getImageData(0, 0, w, h);
-				this.context.putImageData(tmpImageData, x, y);
-			}
-			this.map.iterator.reset();
-			while (this.map.iterator.hasNext()) {
-				area = this.map.nextArea();
-				// console.log(effect, area);
-				if (area.data)
-					_fx.call(this, area.x, area.y, area.w, area.h, area.data);
-			}
-			return this;
-		},
-		update : function() {
-			this.img.src = this.canvas.toDataURL();
-			return this;
-		}
-	};
+var proto = {
+	// handle effect queue
+	fx: function(effect, cells) {
+		// if (!cells)
+			for (var area, next=this.map.iterator(); (area=next());)
+				effect.call(this, area.x, area.y, area.w, area.h, area.data);
+		// else
+		// 	for (var i=0, bound=cells.length; i<bound; i++) {
+		// 		var area = this.map.cell(cells[i]);
+		// 		effect.call(this, area.x, area.y, area.w, area.h, effect, area.data);
+		// 	}
+		return this;
+	},
+	update: function() {
+		this.img.src = this.canvas.toDataURL();
+		return this;
+	}
+};
+for (var member in proto) {
+	Blend.create.prototype[member] = proto[member];
 }
